e35eb4b5e47185e83f7dda8bafe27033180a6c1b
“376305” <shishuaiyun@deppon.com>
2024-01-29 09:48:12 +0800

    chore: first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Button from './components/Button/Button.vue'
+</script>
+
+<template>
+  <main>
+    <Button type="primary" plain>
+      Test Button
+    </Button>
+  </main>
+</template>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { ButtonProps } from './types'
+
+defineProps<ButtonProps>()
+
+defineOptions({
+  name: 'VKButton',
+})
+</script>
+
+<template>
+  <button
+    class="vk-button"
+    :class="{
+      [`vk-button--${type}`]: type,
+      [`vk-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+  >
+    <span><slot /></span>
+  </button>
+</template>
+
+<style scoped>
+
+</style>
